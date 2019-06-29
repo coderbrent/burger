d7602cd31f5c638ab6283ece6f94f6b175b3a516
Brent Abruzese <brent.abruzese@gmail.com>
2019-06-29 10:06:42 -0400

    heroku deployment edited

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 const mysql = require('mysql');
+var connection;
+if(process.env.JAWDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : process.env.DB_PASS,
+    database : 'burgers_db'
+  });
+}
 
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,
-  user     : 'root',
-  password : 'Zsw0wers!',
-  database : 'burgers_db'
-});
  
 connection.connect();
  
